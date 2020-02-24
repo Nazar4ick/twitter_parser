5a7fe601565affac10832d097e8efeb8eec5f899
Nazar4ick <57792397+Nazar4ick@users.noreply.github.com>
2020-02-24 18:40:37 +0200

    Add files via upload

---
+++ hidden.py
@@ -0,0 +1,10 @@
+# Keep this file separate
+
+# https://apps.twitter.com/
+# Create new App and get the four strings
+
+def oauth():
+    return {"consumer_key": "QHKfj3rTVIVhR0G2ZOefalZzD",
+            "consumer_secret": "XBgvXZuw84Mtkxni3AM7fOGEZIbbABDsrzCUB9IBb6YDRxrcCM",
+            "token_key": "1229766040697135104-m7C6BUkyIEG8ZHaWa8K0YdNKY3QY0n",
+            "token_secret": "j0rQbFuQ4wfU20dqFqQbgd1WdOQApDiy8VSUmGdgLqqNH"}
